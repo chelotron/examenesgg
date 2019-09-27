07b33a5e1f7a74381bf70849ddc6a72ac053da25
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 18:26:52 -0400

    CualquierCosa

---
+++ evaluation/webEval/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webEval.Models
+{
+    public class Student
+    {
+        public enum TypeGender {
+
+            Male,
+            Female,
+
+        }
+
+        [Key]
+        public int StudentID { get; set; }
+        [Required(ErrorMessage="Please Enter Name")]
+        public string  Name { get; set; }
+        [Required]
+        public TypeGender Gender { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DataType BirthDate { get; set; }
+
+
+    }
+}
